32fb9e0eea79c5ff60d077739afb05c3a9496180
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-05 16:17:27 -0300

    A base do front-end foi implementada

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+$inc = ['chat', 'index'];
+
+if (!isset($_GET['route']) || !in_array($_GET['route'], $inc)) {
+    header("Location: ?route=index");
+    exit;
+}
+
+$view = $_GET['route'];
+
+require_once "templates/header.php";
+switch($view) {
+    case 'chat' :
+        require_once "inc/chat.php";
+        break;
+    default :
+        require_once "inc/index.php";
+}
+require_once "templates/footer.php";
