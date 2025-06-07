97a401243db51e82888f9713d3657ad676b6363b
Paulo Victor dos Santos Barbosa <paulo.vdbarbosa@gmail.com>
2025-06-07 14:57:34 -0300

    backup db

--- api/inc/db.sql
+++ api/inc/db.sql
@@ -14,11 +14,48 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura do banco de dados para papolive
+CREATE DATABASE IF NOT EXISTS `papolive` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `papolive`;
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela papolive.messages
+CREATE TABLE IF NOT EXISTS `messages` (
+  `uid` varchar(23) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_uid` varchar(23) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `room_uid` varchar(23) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `user_uid` (`user_uid`),
+  KEY `room_uid` (`room_uid`),
+  CONSTRAINT `room_uid` FOREIGN KEY (`room_uid`) REFERENCES `rooms` (`uid`),
+  CONSTRAINT `user_uid` FOREIGN KEY (`user_uid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela papolive.messages: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela papolive.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `uid` varchar(23) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `expired_at` datetime NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela papolive.rooms: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela papolive.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` varchar(23) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela papolive.users: ~0 rows (aproximadamente)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
