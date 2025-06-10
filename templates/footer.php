b58a15dd0bac3ac8fc5ea15b76e1459ec8c1a943
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-10 11:29:02 -0300

    Aqui está a parte em que eu verifico se o user está na parte do chat.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,9 @@
 <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
-<script type="module" src="assets/app.js"></script>
+<?php if (isset($_SESSION['chat.js'])) : ?>
+    <script type="module" src="assets/chat.js"></script>
+<?php else: ?>
+    <script type="module" src="assets/index.js"></script>
+<?php endif; ?>
 
 </body>
 </html>
